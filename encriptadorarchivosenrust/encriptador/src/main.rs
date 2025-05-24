f60c451e98a4f85d15fb9cdc5ccfcb993e84faca
DGmike.dev <dgmike.dev@gmail.com>
2025-05-23 19:46:31 -0600

    fullencryptapplication5

--- encriptadorarchivosenrust/encriptador/src/main.rs
+++ encriptadorarchivosenrust/encriptador/src/main.rs
@@ -101,8 +101,6 @@ pub fn desencriptador_de_archivos(input_path: &str, output_path: &str, key_bytes
     Ok(())
 }
 
-
-
 fn main() {
 
     // 32 caracteres para aes-256
@@ -114,97 +112,3 @@ fn main() {
     encriptador_de_archivos("mensaje.txt", "mensaje.enc", &key, &iv).expect("Falló el cifrado");
     desencriptador_de_archivos("mensaje.enc", "mensaje_decrypted.txt", &key, &iv).expect("Falló el descifrado");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ENCRIPTACION SIMÉTRICA
-// DES, 3DES, AES  AES es la que se recomendo para este proyecto
-// para futuras en criptaciones TLS/SSL, HTTPS, PGP => RSA, ECC
-
-/*
-use aes_gcm::{ aead::{Aead, KeyInit, OsRng}, Aes256Gcm, Nonce};
-
-use base64::{engine::general_purpose, Engine as _};
-
-fn main(){
-
-    // Este debe de ser Random
-    let master = "12341234123412341234123412341234".as_bytes();
-    let cipher = Aes256Gcm::new_from_slice(master).unwrap();
-
-    let nonce_str_base64 = "fvXWsQhmTtDJsGFv";
-    let nonce_str: Vec<u8> = general_purpose::STANDAR.decode(nonce_str_base64).unwrap();
-    let nonce = Nonce::from_slice(&nonce_str_base64);
-
-    // encrypted text from sender
-    let ciphertext_base64 = "Zf5aB0bbVGGX3k9Yt6x+9daxCGZO0MmwYW8VUsOY4j3gNYXP47hvfGgd";
-    let ciphertext = general_purpose::STANDARD.decode(ciphertext_base64).unwrap();
-
-    // gets aead::Error here
-    match cipher.decrypt(nonce, ciphertext.as_slice()) {
-        Ok(decrypted) => {
-            let result = String::from_utf8(decrypted).unwrap();
-            println!("result: {}", result);
-        }
-        Err(err) => print!("{}", err), // <--- prints error: aead::Error
-    };
-
-
-
-} */
-
-
-/*
-
-use aes_gcm::{ aead::{Aead, AeadCore, KeyInit, OsRng}, Aes256Gcm, Nonce, Key };
-
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut path: String = String::from("./outenc/");
-    let mut nombre: &str = "salida.enc";
-
-    // Se genera la llave con valores random
-    // un 
-    let key = Aes256Gcm::generate_key(OsRng);  // KEY
-    let nonce = Aes256Gcm::generate_nonce(&mut OsRng);  // IV
-
-    let cipher = Aes256Gcm::new(&key);
-
-    let ciphertext = cipher.encrypt(&nonce, b"Mensaje Sin Cifrar".as_ref()).map_err(|e| format!("Encrypt error: {:?}", e))?;
-    
-    let plaintext = cipher.decrypt(&nonce, ciphertext.as_ref()).map_err(|e| format!("Decrypt error: {:?}", e))?;
-
-   
-    // println!("nonce: {:?}", nonce);
-
-    match String::from_utf8(ciphertext.to_vec()) {
-        Ok(string) => println!("{}", string),
-        Err(error) => println!("Error: {:?}", error),
-    }
-
-    // println!("Key: {:?}", key);
-    // println!("Mensaje cifrado: {:?}", ciphertext);
-    // println!("Mensaje decifrado: {:?}", plaintext);
-
-    // assert_eq!(&plaintext, b"Mensaje Sin Cifrar");
-
-    Ok(())
-}
-*/
