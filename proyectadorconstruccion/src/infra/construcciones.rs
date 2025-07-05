8559cc59c25fb9369f96f11656c29e93eada9c20
DGmike.dev <dgmike.dev@gmail.com>
2025-07-04 19:59:07 -0600

    addingdosificaciones

---
+++ proyectadorconstruccion/src/infra/construcciones.rs
@@ -0,0 +1,61 @@
+pub struct Losa{
+    largo_mts:             f32,
+    ancho_mts:             f32,
+    alto_mts:              f32,
+    cant_cemento_blt_50kg: f32,
+    cant_arena_m3:         f32,
+    cant_varilla_kg:       f32,
+    cant_varilla_pza:      f32,
+    cant_graba_m3:         f32,
+    cant_agua_lts:         f32
+}
+
+/*
+ Tabla de conversión: m³, kg y L
+Material	        Densidad (kg/m³)	Peso por 1 m³ (kg)	Peso por 1 litro (kg/L)
+Agua	            1,000	            1,000 kg	        1.00 kg/L
+Arena seca	        1,500 – 1,600	    1,500 – 1,600 kg	1.50 – 1.60 kg/L
+Arena húmeda	    1,800 – 2,000	    1,800 – 2,000 kg	1.80 – 2.00 kg/L
+Grava	            1,600 – 1,800	    1,600 – 1,800 kg	1.60 – 1.80 kg/L
+Cemento (a granel)	~1,400	            ~1,400 kg	        ~1.40 kg/L
+Concreto fresco	    2,300 – 2,500	    2,300 – 2,500 kg	2.30 – 2.50 kg/L
+Tierra suelta	    1,200 – 1,400	    1,200 – 1,400 kg	1.20 – 1.40 kg/L
+*/
+
+
+
+// bote de agua * 20 = lts
+// bote arena * 52 = 1 mt3
+
+/*
+Paso 1: Conocer la densidad de la grava
+
+La densidad de la grava (seca y limpia) varía, pero en promedio es:
+
+    1,500 a 1,800 kg/m³
+
+Vamos a usar un valor intermedio:
+
+    1,600 kg/m³ (es decir, 1.6 kg por litro)
+
+✅ Paso 2: Convertir litros a metros cúbicos
+
+Sabemos que:
+
+    1 m³ = 1,000 litros
+
+    Entonces, 20 litros = 0.02 m³
+
+    */
+
+
+
+
+
+
+
+
+
+pub struct Dosificacion{
+
+}
--- proyectadorconstruccion/src/main.rs
+++ proyectadorconstruccion/src/main.rs
@@ -1,3 +1,110 @@
+// DOSIFICACIÓN CEMENTO MOCTEZUMA
+// https://www.academia.edu/37266141/TABLA_DOSIFICADORA_CPC_30_R
+
+
+
+// RESISTENCIA DE 100Kg / cm2
+// MUROS, PISOS, FIRMES, BANQUETAS
+// 1 BLTO, 
+// 2.5 BTS DE AGUA 
+// 7 BTS DE ARENA
+// 8 BTS DE GRABA
+// 1 mt3 EL FACTOR ES X 5
+
+
+// RESISTENCIA DE 150 Kg/cm2
+// TRABES, CASTILLOS, DALAS, CADENAS
+// 1 BLTO
+// 2 BTS DE AGUA
+// 5.5 BTS DE ARENA
+// 6.5 BTS DE GRABA
+// 1 mt3 EL FACTOR ES POR 6
+
+// RESISTENCIA DE 200 Kg/cm2
+// LOSAS, ENTREPISOS, TRABES, ZAPATAS
+// 1 BLTO
+// 7.75 BTS DE AGUA
+// 4.5 BTS DE ARENA
+// 5.5 BTS DE GRABA
+// 1 mt3 EL FACTOR ES POR 7
+
+// RESISTENCIA DE 250 Kg/cm2
+// COLUMNAS Y LOSAS ESPECIALES
+// 1 BLTO
+// 1.5 BTS DE AGUA
+// 3.5 BTS DE ARENA
+// 5 BTS DE GRABA
+// 1 mt3 EL FACTOR ES POR 8
+
+// RESISTENCIA DE 300 Kg/cm2
+// COLUMNAS Y LOSAS ESPECIALES
+// 1 BLTO
+// 1.25 BTS DE AGUA
+// 2.5 BTS DE ARENA
+// 4 BTS DE GRABA
+// 1 mt3 EL FACTOR ES POR 9
+
+
+
+#[derive(Debug)]
+pub struct Losa{
+    largo_mts:             f32,
+    ancho_mts:             f32,
+    alto_mts:              f32,
+    cant_cemento_blt_50kg: f32,
+    cant_arena_m3:         f32,
+    cant_varilla_kg:       f32,
+    cant_varilla_pza:      f32,
+    cant_graba_m3:         f32,
+    cant_agua_lts:         f32
+}
+
+impl Losa{
+
+    pub fn new(_largo:f32, _ancho:f32, _alto:f32)->Losa{
+        let losa: Losa = Losa {
+            largo_mts : _largo,
+            ancho_mts : _ancho,
+            alto_mts  : _alto,
+            cant_cemento_blt_50kg  : (_largo * _ancho * _alto ) * 7.5,          // numero de bultos de 50 Kg
+            cant_varilla_kg : (_largo * _ancho) * 7.0,                          // Kilos de varilla    1 varilla de 3/8 = 6.6kg
+            cant_varilla_pza:  ( (_largo * _ancho) * 7.0 ) / 6.6,               // varilla por pieza
+            cant_arena_m3 : ( (_largo * _ancho * _alto ) * 7.5 ) * 4.0 * 0.02,  // m3 de arena
+            cant_graba_m3 : ( (_largo * _ancho * _alto ) * 7.5 ) * 5.0 * 0.02,  // m3 de graba
+            cant_agua_lts :  ( (_largo * _ancho * _alto ) * 7.5 ) * 40.0        // Lts de agua
+        };
+        losa
+    }
+
+    pub fn tolerancias(self, tol: f32)-> Vec<f32>{
+        let tol_cant_cemento_blt_50kg =  self.cant_cemento_blt_50kg +  ( (self.cant_cemento_blt_50kg * tol ) / 100.0);
+        let tol_cant_varilla_kg =        self.cant_varilla_kg +  ( (self.cant_varilla_kg * tol ) / 100.0);
+        let tol_cant_varilla_pza =       self.cant_varilla_pza +  ( (self.cant_varilla_pza * tol ) / 100.0);
+        let tol_cant_arena_m3 =          self.cant_arena_m3 +  ( (self.cant_arena_m3 * tol ) / 100.0);
+        let tol_cant_graba_m3 =          self.cant_graba_m3 +  ( (self.cant_graba_m3 * tol ) / 100.0);
+        let tol_cant_agua_lts =          self.cant_agua_lts +  ( (self.cant_agua_lts * tol ) / 100.0);
+
+        let res: Vec<f32> = vec![
+            tol_cant_cemento_blt_50kg ,
+            tol_cant_varilla_kg ,
+            tol_cant_varilla_pza ,
+            tol_cant_arena_m3 ,
+            tol_cant_graba_m3 ,
+            tol_cant_agua_lts ]
+        ;
+        res
+    }
+}
+
+fn main(){
+    let losa = Losa::new(20.0,10.0,0.10);
+    println!("{:?}",losa);
+
+    println!("{:?}", losa.tolerancias(5.0));
+}
+
+
+/*
 pub struct Escalera{
     altura: f32,
     longitud:  f32,
@@ -25,6 +132,8 @@ impl Escalera{
     }
 }
 
+
+
 fn main() {
 
     let escalera: (f32, f32, f32, f32, f32, f32) = 
@@ -37,3 +146,5 @@ fn main() {
 
     print!("\nAltura: {}\nLongitud: {}\n#Huellas: {}\n#Contra huellas: {}\nHuellas x Longitud: {}\nCHuellas x altura: {}", escalera.0,escalera.1,escalera.2,escalera.3, escalera.4, escalera.5);
 }
+
+*/
